09973f1b592ae4855401c64d36e9e69f53d7b1ca
Raph <raphaelito74@gmail.com>
2025-10-29 23:59:17 +0100

    V1.0.1

--- server/routes/interactions.js
+++ server/routes/interactions.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { addLike, removeLike, sharePost } from ('../../scripts/social/interactions');
+import { addLike, removeLike, sharePost } from '../../scripts/social/interactions';
 
 // Route to like a post
 router.post('/like/:postId', async (req, res) => {
